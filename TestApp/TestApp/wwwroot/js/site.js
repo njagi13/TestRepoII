b3d01f61bd22895ba2dce36280d75269bd108e68
dnjagi <njagi.ndirangu@icealion.com>
2021-06-13 06:44:38 +0300

    Initial commit

---
+++ TestApp/TestApp/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+function SumShoesInBag(x, y) {
+    return x - y;
+}
+
+function SabtctShoesInBag(x, y) {
+    return x - y;
+}
+
+//We are on the dev br
+
+/// Here we will have a new login page 
